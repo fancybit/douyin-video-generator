e579b1d9ff05cabf79c07f60cf815361a8173d0c
fancybit <fancybit@qq.com>
2026-06-01 00:18:09 +0800

    fix: replace auth-ui with plain form, remove SSR-incompatible deps

--- web/app/login/page.tsx
+++ web/app/login/page.tsx
@@ -2,7 +2,7 @@ import dynamic from 'next/dynamic'
 
 const LoginForm = dynamic(() => import('./LoginForm'), { ssr: false })
 
-export const dynamic = 'force-static'
+export const dynamic = 'force-dynamic'
 
 export default function LoginPage() {
   return <LoginForm />
